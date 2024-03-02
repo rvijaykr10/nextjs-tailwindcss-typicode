d0ca9cf203c9f80c3c60fc7a78ff4eafb7afaabf
vijaykumar <vijaykumar@vijaykumars-MacBook-Air.local>
2024-03-02 14:30:03 +0530

    heading changed

--- components/Posts.jsx
+++ components/Posts.jsx
@@ -3,15 +3,13 @@ import React from "react";
 const Posts = ({ posts }) => {
   return (
     <div className="px-2">
-      <h2 className="text-center text-xl underline underline-offset-2 font-semibold pb-4">
-        POSTS
-      </h2>
+      <div className="flex justify-center items-center h-10">Posts!</div>
       {posts?.map((obj) => (
-        <div className="mb-2">
-          <h2 key={obj.id}>
+        <div className="mb-2" key={obj?.id}>
+          <h2>
             <span className="text-base font-normal italic">- {obj?.title}</span>
           </h2>
-          <p key={obj.id}>
+          <p>
             <span className="text-sm font-light italic pl-2">{obj?.body}</span>
           </p>
         </div>
--- components/Users.jsx
+++ components/Users.jsx
@@ -4,15 +4,13 @@ const Users = ({ users }) => {
   console.log(users);
   return (
     <div className="px-2">
-      <h2 className="text-center text-xl underline underline-offset-2 font-semibold pb-4">
-        USERS
-      </h2>
+      <div className="flex justify-center items-center h-10">Users!</div>
       {users?.map((obj) => (
-        <div className="mb-2">
-          <h2 key={obj.id}>
+        <div key={obj.id} className="mb-2">
+          <h2>
             <span className="text-base font-normal italic">- {obj?.name}</span>
           </h2>
-          <p key={obj.id}>
+          <p>
             <span className="text-sm font-light italic pl-2">{obj?.email}</span>
           </p>
         </div>
